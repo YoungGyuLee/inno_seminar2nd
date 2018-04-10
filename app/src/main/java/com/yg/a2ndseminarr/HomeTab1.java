0b97c3be4ef3405bdbe6c7f7f3730e6f2bb5eb7e
YoungGyuLee <sket8993@gmail.com>
2018-04-10 15:31:49 +0900

    2ndSeminar

---
+++ app/src/main/java/com/yg/a2ndseminarr/HomeTab1.java
@@ -0,0 +1,41 @@
+package com.yg.a2ndseminarr;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeTab1 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+      //  View v;
+       // v = inflater.inflate~~~~~
+
+//        HomeTab fragment;
+//        fragment = new HomeTab();
+//       // addFragment(new HomeTab());
+    }
+
+    int add(int a1, int a2){
+        return a1 + a2;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
